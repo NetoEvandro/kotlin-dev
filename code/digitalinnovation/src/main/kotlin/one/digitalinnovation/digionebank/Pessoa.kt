fc62c08e5373798c544f188f4dca3f83c6bcc08f
NetoEvandro <netoevandro@hotmail.com>
2021-07-09 15:37:59 -0300

    Var cpf - private set

--- code/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ code/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,13 +1,15 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var nome:String = "Evandro"
-    var cpf:String = "123.456.78-90"
+    var nome: String = "Evandro"
+
+    var cpf: String = "123.456.78-90"
+    private set
 }
 
 fun main() {
     val evandro = Pessoa()
 
-    println("Nome: "+ evandro.nome)
-    println("Cpf: "+ evandro.cpf)
+    println("Nome: " + evandro.nome)
+    println("Cpf: " + evandro.cpf)
 }
