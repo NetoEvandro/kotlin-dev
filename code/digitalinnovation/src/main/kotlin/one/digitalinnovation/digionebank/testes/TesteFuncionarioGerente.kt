c245b25c934275b160aaa913ff1969480d234437
NetoEvandro <netoevandro@hotmail.com>
2021-07-21 23:00:53 -0300

    Subindo curso kotlin

--- code/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
+++ code/digitalinnovation/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,6 +4,8 @@ import one.digitalinnovation.digionebank.Gerente
 
 fun main() {
 
-    val maria = Gerente("Maria", "111.222.333-44", 3000.0)
+    val maria = Gerente("Maria", "111.222.333-44", 3000.0, "senha123")
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
