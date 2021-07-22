741097611e1657786e213737bdc301d46c03b09e
NetoEvandro <netoevandro@hotmail.com>
2021-07-22 00:39:57 -0300

    Teste add calculadora

---
+++ code/calculadora/teste.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main() {
+
+    val input = Scanner(System.`in`)
+
+    print("Digite o primeiro número:")
+    val a = input.nextInt()
+    print("Digite o segundo número:")
+    val b = input.nextInt()
+
+    soma = a + b
+
+    println("SOMA = "+ soma)  //insira as variáveis corretamente
+}
