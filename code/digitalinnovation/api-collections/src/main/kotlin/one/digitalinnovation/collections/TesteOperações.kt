362ed307c178ffe55ae53c3d0a2f9e6727b1b1d0
NetoEvandro <netoevandro@hotmail.com>
2021-07-26 11:16:42 -0300

    Alteração da classe TesteOperações.kt

--- code/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperações.kt
+++ code/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteOperações.kt
@@ -6,7 +6,7 @@ fun main() {
     for (valores in salarios) {
         println(valores)
     }
-    println("--------------------")
+    println("----------------------------------------")
     println("Maior que salário: ${salarios.maxOrNull()}")
     println("Menor salário: ${ salarios.minOrNull()}")
     println("Média de salários: ${salarios.average()}")
@@ -14,9 +14,20 @@ fun main() {
     val salariosNaiorQue2500 = salarios.filter {
         it >= 2500.0
     }
-    println("--------------------")
+    println("----------------------------------------")
     salariosNaiorQue2500.forEach {
         println("Salários maiores que R$2.500,00")
         println(it)
     }
+
+    println("-----------------------------------------")
+    println("Salários entre R$2.000,00 e 5.000,00:  ${salarios.count{it in 2000.0 .. 5000.0}}")
+
+    println("-----------------------------------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("-----------------------------------------")
+    println(salarios.any { it == 1000.0 })
+    println(salarios.any{ it == 500.0 })
 }
