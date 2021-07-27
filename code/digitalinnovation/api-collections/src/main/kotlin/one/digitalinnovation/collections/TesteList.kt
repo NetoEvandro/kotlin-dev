f10ea620efe59a9297161c6ecf712b38388e314f
NetoEvandro <netoevandro@hotmail.com>
2021-07-27 10:27:45 -0300

    Alteração da classe TesteList.kt

--- code/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
+++ code/digitalinnovation/api-collections/src/main/kotlin/one/digitalinnovation/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package one.digitalinnovation.collections
 
 fun main() {
-    val joao = Funcionario ("João", 1000.0)
-    val pedro = Funcionario ("Pedro", 2000.0)
-    val maria = Funcionario ("Maria", 4000.0)
+    val joao = Funcionario ("João", 2000.0, "CLT")
+    val pedro = Funcionario ("Pedro", 1500.0, "PJ")
+    val maria = Funcionario ("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -11,12 +11,25 @@ fun main() {
         println(it)
     }
     //Testando procurar funcionario na list pelo nome
+    println("-------------------")
     println(funcionarios.find { it.nome == "Maria"})
+
+    println("-------------------")
+    //Ordedando obj funcionario pelo salário
+    funcionarios
+        .sortedBy { it.salario }
+        .forEach{ println(it)}
+
+    println("-------------------")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach{ println(it)}
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
 override fun toString(): String =
     """
